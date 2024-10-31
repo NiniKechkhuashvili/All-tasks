67f1829a0292c7df97ce8b5a3c66746311c23164
nkechkhuashvili <nkechkhuashvili@LHO10076724>
2024-10-31 22:46:47 +0400

    HomeWorkN5

---
+++ HomeWorkN5/HomeWorkN5/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace HomeWorkN5
+{
+    internal class Program
+    {
+        static void Main()
+
+        {
+            Console.WriteLine("write a whole number");
+            string[] input = Console.ReadLine().Split(' ');
+            int[] array = Array.ConvertAll(input, int.Parse);
+
+
+            Console.WriteLine("write an index");
+            int index = int.Parse(Console.ReadLine());
+
+
+            int result = Sum(array, index);
+            Console.WriteLine($"sum of numbers for index {index}: {result}");
+        }
+
+        static int Sum(int[] array, int index)
+        {
+            if (index < 0 || index >= array.Length)
+            {
+                Console.WriteLine("Index");
+                return 0;
+            }
+
+            int number = array[index];
+            int sum = 0;
+
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            return sum;
+        }
+    }
+
+
+}
