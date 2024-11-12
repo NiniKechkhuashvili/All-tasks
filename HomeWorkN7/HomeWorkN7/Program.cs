26bd74b9689befb6b031fbc7674adc3a2b7170ec
nkechkhuashvili <nkechkhuashvili@LHO10076724>
2024-11-12 22:48:15 +0400

    HomeWorkN7

---
+++ HomeWorkN7/HomeWorkN7/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HomeWorkN7
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string personName = "Nini";
+            int personAge = 25;
+
+            Person person1 = new Person();
+            person1.Name = personName;
+            person1.Age = personAge;
+
+            Person person2 = person1;
+
+            person2.Name = "Nana";
+            person2.Age = 35;
+
+            Console.WriteLine($"person1 - Name: {person1.Name}, Age: {person1.Age}");
+            Console.WriteLine($"person2 - Name: {person2.Name}, Age: {person2.Age}");
+        }
+    }
+
+}
